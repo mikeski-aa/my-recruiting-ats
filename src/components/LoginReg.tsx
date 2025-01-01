5dedbb5c922f98676083987661f8e57cc5b48582
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2025-01-01 13:06:03 +0100

    Working on login page setup

--- src/components/LoginReg.tsx
+++ src/components/LoginReg.tsx
@@ -3,9 +3,29 @@ import { useState } from "react";
 function LoginReg() {
   const [screen, setScreen] = useState<boolean>(false);
 
+  const handleLoginSwitch = () => {
+    setScreen(!screen);
+  };
+
   return (
     <div className="logregcontainer">
-      <div className={screen ? "login show" : "login hide"}></div>
+      <div className={screen ? "login show" : "login hide"}>
+        <div className="inputHolder">
+          <label htmlFor="usernameInput">Username</label>
+          <input id="usernameInput" type="text"></input>
+        </div>
+        <div className="inputHolder">
+          <label htmlFor="passwordInput">Username</label>
+          <input id="passwordInput" type="password"></input>
+        </div>
+        <div className="buttonHolderLogin">
+          <button className="loginButton">Login</button>
+          <button className="loginButton" onClick={handleLoginSwitch}>
+            Register
+          </button>
+          <button className="loginButton">Guest Login</button>
+        </div>
+      </div>
       <div className={!screen ? "register show" : "register hide"}></div>
     </div>
   );
