dcbc63233beab68c1630db0c4303c7f6e181a942
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2025-01-01 16:24:22 +0100

    working on login setup

--- src/components/LoginReg.tsx
+++ src/components/LoginReg.tsx
@@ -11,8 +11,8 @@ function LoginReg() {
     <div className="logregcontainer">
       <div className={screen ? "login show" : "login hide"}>
         <div className="inputHolder">
-          <label htmlFor="usernameInput">Username</label>
-          <input id="usernameInput" type="text"></input>
+          <label htmlFor="usernameInput">Email</label>
+          <input id="usernameInput" type="email"></input>
         </div>
         <div className="inputHolder">
           <label htmlFor="passwordInput">Username</label>
@@ -26,7 +26,27 @@ function LoginReg() {
           <button className="loginButton">Guest Login</button>
         </div>
       </div>
-      <div className={!screen ? "register show" : "register hide"}></div>
+      <div className={!screen ? "register show" : "register hide"}>
+        <div className="inputHolder">
+          <label htmlFor="emailInput">Email</label>
+          <input id="emailInput" type="email"></input>
+        </div>
+        <div className="inputHolder">
+          <label htmlFor="passwordInput">Username</label>
+          <input id="passwordInput" type="password"></input>
+        </div>
+        <div className="inputHolder">
+          <label htmlFor="passwordInputConfirm">Username</label>
+          <input id="passwordInputConfirm" type="password"></input>
+        </div>
+        <div className="buttonHolderLogin">
+          <button className="loginButton">Login</button>
+          <button className="loginButton" onClick={handleLoginSwitch}>
+            Register
+          </button>
+          <button className="loginButton">Guest Login</button>
+        </div>
+      </div>
     </div>
   );
 }
