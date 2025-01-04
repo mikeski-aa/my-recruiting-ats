3eb6b9a110de2550ba5d062b4eae1c3f62966299
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2025-01-04 11:48:39 +0100

    Working on setting up the diff pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import LoginReg from "./components/LoginReg";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import NewReq from "./components/NewReq";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout></Layout>,
-      children: [{ index: true, element: <Home></Home> }],
+      children: [
+        { index: true, element: <Home></Home> },
+        { path: "/newreq", element: <NewReq /> },
+      ],
     },
     {
       path: "/login",
