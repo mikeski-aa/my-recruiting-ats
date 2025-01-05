810b2897f50d129761c694456f0d45f2bd54de34
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2025-01-05 11:04:46 +0100

    xd

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,23 @@
+import { useNavigate } from "react-router";
 import "../styles/nav.css";
 
 function Nav() {
+  const navigate = useNavigate();
+
+  const handleNewReqClick = () => {
+    navigate("/newreq");
+  };
+  const handleHomeClick = () => {
+    navigate("/");
+  };
   return (
     <div className="navBar">
       <div className="leftSide">
         <h1>banner</h1>
       </div>
       <div className="rightSide">
-        <button>button</button>
-        <button>button</button>
+        <button onClick={handleHomeClick}>Home</button>
+        <button onClick={handleNewReqClick}>Create new req</button>
         <button>button</button>
         <button>button</button>
       </div>
