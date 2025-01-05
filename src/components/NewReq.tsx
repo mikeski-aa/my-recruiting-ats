c04ffa06ed399e3ed862c78cee215ed6ba8657d7
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2025-01-05 11:15:11 +0100

    Working on new role creation

--- src/components/NewReq.tsx
+++ src/components/NewReq.tsx
@@ -1,3 +1,5 @@
+import "../styles/newreq.css";
+
 function NewReq() {
   return (
     <div className="createNewReq">
@@ -7,16 +9,28 @@ function NewReq() {
           <input id="reqName" type="text"></input>
         </div>
         <div className="reqCreationHolder">
-          <label htmlFor="reqName">Requisition Name</label>
+          <label htmlFor="reqOrg">Job Org Family</label>
+          <select id="reqOrg">
+            <option selected={true}>Select one from below</option>
+            <option>Engineering</option>
+            <option>Product</option>
+            <option>People</option>
+            <option>Finance</option>
+            <option>Legal</option>
+            <option>Operations</option>
+          </select>
+        </div>
+        <div className="reqCreationHolder">
+          <label htmlFor="reqDesc">Requisition description</label>
+          <textarea id="reqName"></textarea>
+        </div>
+        <div className="reqCreationHolder">
+          <label htmlFor="hiringManager">Hiring manager</label>
           <input id="reqName" type="text"></input>
         </div>
         <div className="reqCreationHolder">
-          <label htmlFor="reqName">Requisition Name</label>
-          <input id="reqName" type="text"></input>
-        </div>
-        <div className="reqCreationHolder">
-          <label htmlFor="reqName">Requisition Name</label>
-          <input id="reqName" type="text"></input>
+          <label htmlFor="taManager">Recruiter assigned</label>
+          <input id="taManager" type="text"></input>
         </div>
       </div>
     </div>
