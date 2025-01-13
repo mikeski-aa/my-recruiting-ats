31c94a759a8f5dbf5ffb5364625df0dcb4a06acb
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2025-01-13 22:56:40 +0100

    added holders

--- src/components/NewReq.tsx
+++ src/components/NewReq.tsx
@@ -18,31 +18,37 @@ function NewReq() {
           <label htmlFor="reqName">Requisition Name</label>
           <input id="reqName" type="text"></input>
         </div>
-        <div className="reqCreationHolder">
-          <label htmlFor="reqOrg">Job Org Family</label>
-          <select id="reqOrg">
-            <option selected={true}>Select one from below</option>
-            <option>Engineering</option>
-            <option>Product</option>
-            <option>People</option>
-            <option>Finance</option>
-            <option>Legal</option>
-            <option>Operations</option>
-            <option>Other</option>
-          </select>
+        <div className="reqCreationOne">
+          <div className="reqCreationHolder">
+            <label htmlFor="reqOrg">Job Org Family</label>
+            <select id="reqOrg">
+              <option selected={true}>Select one from below</option>
+              <option>Engineering</option>
+              <option>Product</option>
+              <option>People</option>
+              <option>Finance</option>
+              <option>Legal</option>
+              <option>Operations</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="reqCreationHolder">
+            <label htmlFor="reqDesc">Requisition description</label>
+            <textarea id="reqName"></textarea>
+          </div>
+          <div className="reqCreationHolder">
+            <label htmlFor="hiringManager">Hiring manager</label>
+            <input id="reqName" type="text"></input>
+          </div>
+          <div className="reqCreationHolder">
+            <label htmlFor="taManager">Recruiter assigned</label>
+            <input id="taManager" type="text"></input>
+          </div>
+          <div className="reqCreationTwo">
+            <div className="reqCreationHolder"></div>
+          </div>
         </div>
-        <div className="reqCreationHolder">
-          <label htmlFor="reqDesc">Requisition description</label>
-          <textarea id="reqName"></textarea>
-        </div>
-        <div className="reqCreationHolder">
-          <label htmlFor="hiringManager">Hiring manager</label>
-          <input id="reqName" type="text"></input>
-        </div>
-        <div className="reqCreationHolder">
-          <label htmlFor="taManager">Recruiter assigned</label>
-          <input id="taManager" type="text"></input>
-        </div>
+
         <button className="submitReq" onClick={handleBackClick}>
           Back
         </button>
